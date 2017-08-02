9e677aab60d8b3271c7444ec3a4287ca0af45323
efekta <efekta9@gmail.com>
2017-08-02 11:38:09 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 //SIDEBAR OPEN
 
-var sidebarLink = document.querySelector('.sidebar-link');
-var sidebar = document.querySelector(".o-sidebar");
-var closeSidebar = document.querySelector(".sidebar__link-close");
-sidebarLink.addEventListener("click", function(event) {
-	event.preventDefault();
-	sidebar.classList.add("o-sidebar-open");
+// var sidebarLink = document.querySelector('.sidebar-link');
+// var sidebar = document.querySelector(".o-sidebar");
+// var closeSidebar = document.querySelector(".sidebar__link-close");
+// sidebarLink.addEventListener("click", function(event) {
+// 	event.preventDefault();
+// 	sidebar.classList.add("o-sidebar-open");
+// });
+
+
+// closeSidebar.addEventListener("click", function(event) {
+// event.preventDefault();
+// sidebar.classList.remove("o-sidebar-open");
+// });
+
+
+jQuery(document).ready(function($){ 
+ $('.sidebar-link,.sidebar__link-close').on('click', function() {
+     if($('body').hasClass('fixed')) {
+      $('body').removeClass('fixed');
+     }else{
+      $('body').addClass('fixed');
+     }
+     return false;
+ });
 });
-
-
-closeSidebar.addEventListener("click", function(event) {
-event.preventDefault();
-sidebar.classList.remove("o-sidebar-open");
-});
-
-
