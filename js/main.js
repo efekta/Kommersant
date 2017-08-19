c5c8bbd6e7ea87705524ea0de50d9eaad736c68e
efekta <efekta9@gmail.com>
2017-08-19 21:29:16 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -108,6 +108,13 @@ $(function() {
     $("#datepicker").datepicker($.datepicker.regional["ru"]);
 });
 
+ $('.qtyplus').click(function(e) {
+   var $target = $(e.currentTarget),
+       $parent = $target.closest('.count-item-num'),
+       $num = $parent.find('.qty')
+       currentNum = parseInt($num.val(), 10) || 0;
+   $num.val(++currentNum);
+  });
 //
 // $(function() {
 //   // This button will increment the value
@@ -184,42 +191,43 @@ $(function() {
 // });
 
 
-$(function() {
-  // This button will increment the value
-  $('.qtyplus').click(function(e) {
-    // Stop acting like a button
-    e.preventDefault();
-    // Get the field name
-    fieldName = $(this).attr('field');
-    // Get its current value
-    var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-    // If is not undefined
-    if (!isNaN(currentVal)) {
-      // Increment
-      $('input[name=' + fieldName + ']').val(currentVal + 1);
-    } else {
-      // Otherwise put a 0 there
-      $('input[name=' + fieldName + ']').val(0);
-    }
-  });
-  // This button will decrement the value till 0
-  $(".qtyminus").click(function(e) {
-    // Stop acting like a button
-    e.preventDefault();
-    // Get the field name
-    fieldName = $(this).attr('field');
-    // Get its current value
-    var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-    // If it isn't undefined or its greater than 0
-    if (!isNaN(currentVal) && currentVal > 0) {
-      // Decrement one
-      $('input[name=' + fieldName + ']').val(currentVal - 1);
-    } else {
-      // Otherwise put a 0 there
-      $('input[name=' + fieldName + ']').val(0);
-    }
-  });
-});
+
+// $(function() {
+//   // This button will increment the value
+//   $('.qtyplus').click(function(e) {
+//     // Stop acting like a button
+//     e.preventDefault();
+//     // Get the field name
+//     fieldName = $(this).attr('field');
+//     // Get its current value
+//     var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+//     // If is not undefined
+//     if (!isNaN(currentVal)) {
+//       // Increment
+//       $('input[name=' + fieldName + ']').val(currentVal + 1);
+//     } else {
+//       // Otherwise put a 0 there
+//       $('input[name=' + fieldName + ']').val(0);
+//     }
+//   });
+//   // This button will decrement the value till 0
+//   $(".qtyminus").click(function(e) {
+//     // Stop acting like a button
+//     e.preventDefault();
+//     // Get the field name
+//     fieldName = $(this).attr('field');
+//     // Get its current value
+//     var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+//     // If it isn't undefined or its greater than 0
+//     if (!isNaN(currentVal) && currentVal > 0) {
+//       // Decrement one
+//       $('input[name=' + fieldName + ']').val(currentVal - 1);
+//     } else {
+//       // Otherwise put a 0 there
+//       $('input[name=' + fieldName + ']').val(0);
+//     }
+//   });
+// });
 //PARALLAX
 
 // let $container = $('.parallax'),
